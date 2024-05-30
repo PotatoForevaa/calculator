014fcbe0da948a9babf6b01c901dedc14365e1c4
Kirill Muzykantov <156402219+PotatoForevaa@users.noreply.github.com>
2024-05-30 21:57:21 +0500

    Add files via upload

---
+++ res_rc.py
@@ -0,0 +1,267 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.7.0
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x09x\
+<\
+?xml version=\x221.\
+0\x22 encoding=\x22UTF\
+-8\x22 standalone=\x22\
+no\x22 ?>\x0a<!DOCTYPE\
+ svg PUBLIC \x22-//\
+W3C//DTD SVG 1.1\
+//EN\x22 \x22http://ww\
+w.w3.org/Graphic\
+s/SVG/1.1/DTD/sv\
+g11.dtd\x22>\x0a<svg x\
+mlns=\x22http://www\
+.w3.org/2000/svg\
+\x22 xmlns:xlink=\x22h\
+ttp://www.w3.org\
+/1999/xlink\x22 ver\
+sion=\x221.1\x22 width\
+=\x22800\x22 height=\x228\
+00\x22 viewBox=\x220 0\
+ 800 800\x22 xml:sp\
+ace=\x22preserve\x22>\x0a\
+<desc>Created wi\
+th Fabric.js 5.3\
+.0</desc>\x0a<defs>\
+\x0a</defs>\x0a<g tran\
+sform=\x22matrix(1.\
+5625 0 0 1.5625 \
+400 400)\x22 id=\x22Qe\
+ipjqmITiSHuf1Kdt\
+0fF\x22  >\x0a<path st\
+yle=\x22stroke: non\
+e; stroke-width:\
+ 1; stroke-dasha\
+rray: none; stro\
+ke-linecap: butt\
+; stroke-dashoff\
+set: 0; stroke-l\
+inejoin: miter; \
+stroke-miterlimi\
+t: 4; fill: rgb(\
+255,255,255); fi\
+ll-rule: nonzero\
+; opacity: 1;\x22  \
+transform=\x22 tran\
+slate(-256, -256\
+)\x22 d=\x22M 459.279 \
+63.989 L 193.251\
+ 63.989 C 172.90\
+5 63.989 153.551\
+ 72.809 140.197 \
+88.175 L 4.314 2\
+44.473 C -1.4379\
+999999999997 251\
+.08300000000003 \
+-1.4379999999999\
+997 260.916 4.31\
+4 267.533 L 140.\
+197 423.83900000\
+000006 C 153.551\
+ 439.19800000000\
+004 172.905 448.\
+0110000000001 19\
+3.251 448.011000\
+0000001 L 459.27\
+9 448.0110000000\
+001 C 488.395 44\
+8.0110000000001 \
+512 424.40600000\
+000006 512 395.2\
+900000000001 L 5\
+12 116.716 C 512\
+ 87.593 488.395 \
+63.989 459.279 6\
+3.989 z M 360.58\
+1 338.701 L 308.\
+894 287.029 L 25\
+7.222 338.687 L \
+226.191999999999\
+98 307.672 L 277\
+.865 255.985 L 2\
+26.2 204.32 L 25\
+7.222 173.298 L \
+308.894 224.971 \
+L 360.573 173.28\
+4 L 391.59499999\
+999997 204.32 L \
+339.907999999999\
+96 255.999 L 391\
+.609 307.672 L 3\
+60.581 338.701 z\
+\x22 stroke-linecap\
+=\x22round\x22 />\x0a</g>\
+\x0a<g transform=\x22m\
+atrix(2.43 2.43 \
+-0.56 0.56 482.5\
+05399568 400.000\
+0021635)\x22 id=\x22H-\
+yz9m0Z5NGwN9uAea\
+tK4\x22  >\x0a<path st\
+yle=\x22stroke: rgb\
+(10,75,124); str\
+oke-width: 0; st\
+roke-dasharray: \
+none; stroke-lin\
+ecap: butt; stro\
+ke-dashoffset: 0\
+; stroke-linejoi\
+n: miter; stroke\
+-miterlimit: 4; \
+fill: rgb(71,71,\
+71); fill-rule: \
+nonzero; opacity\
+: 1;\x22  transform\
+=\x22 translate(0, \
+0)\x22 d=\x22M -43.196\
+54 -43.19654 L 4\
+3.19655 -43.1965\
+4 L 43.19655 43.\
+19655 L -43.1965\
+4 43.19655 z\x22 st\
+roke-linecap=\x22ro\
+und\x22 />\x0a</g>\x0a<g \
+transform=\x22matri\
+x(-2.43 2.43 -0.\
+56 -0.56 482.505\
+4017316 400)\x22 id\
+=\x22Fz5ZaNWa0brK_-\
+KLj8N71\x22  >\x0a<pat\
+h style=\x22stroke:\
+ rgb(10,75,124);\
+ stroke-width: 0\
+; stroke-dasharr\
+ay: none; stroke\
+-linecap: butt; \
+stroke-dashoffse\
+t: 0; stroke-lin\
+ejoin: miter; st\
+roke-miterlimit:\
+ 4; fill: rgb(71\
+,71,71); fill-ru\
+le: nonzero; opa\
+city: 1;\x22  trans\
+form=\x22 translate\
+(-0.000005, -0.0\
+00005)\x22 d=\x22M -43\
+.19654 -43.19654\
+ L 43.19655 -43.\
+19654 L 43.19655\
+ 43.19655 L -43.\
+19654 43.19655 z\
+\x22 stroke-linecap\
+=\x22round\x22 />\x0a</g>\
+\x0a</svg>\
+\x00\x00\x04\x12\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22  viewBox=\
+\x220 0 50 50\x22 widt\
+h=\x2250px\x22 height=\
+\x2250px\x22><path d=\x22\
+M 14 4 C 8.48866\
+61 4 4 8.4886661\
+ 4 14 L 4 36 C 4\
+ 41.511334 8.488\
+6661 46 14 46 L \
+36 46 C 41.51133\
+4 46 46 41.51133\
+4 46 36 L 46 14 \
+C 46 8.4886661 4\
+1.511334 4 36 4 \
+L 14 4 z M 18 10\
+ L 32 10 C 34.20\
+9 10 36 11.791 3\
+6 14 L 36 36 C 3\
+6 38.209 34.209 \
+40 32 40 L 18 40\
+ C 15.791 40 14 \
+38.209 14 36 L 1\
+4 14 C 14 11.791\
+ 15.791 10 18 10\
+ z M 18 13 C 17.\
+448 13 17 13.448\
+ 17 14 L 17 18 C\
+ 17 18.552 17.44\
+8 19 18 19 L 32 \
+19 C 32.552 19 3\
+3 18.552 33 18 L\
+ 33 14 C 33 13.4\
+48 32.552 13 32 \
+13 L 18 13 z M 1\
+9 21 A 2 2 0 0 0\
+ 19 25 A 2 2 0 0\
+ 0 19 21 z M 25 \
+21 A 2 2 0 0 0 2\
+5 25 A 2 2 0 0 0\
+ 25 21 z M 31 21\
+ A 2 2 0 0 0 31 \
+25 A 2 2 0 0 0 3\
+1 21 z M 19 27 A\
+ 2 2 0 0 0 19 31\
+ A 2 2 0 0 0 19 \
+27 z M 25 27 A 2\
+ 2 0 0 0 25 31 A\
+ 2 2 0 0 0 25 27\
+ z M 31 27 A 2 2\
+ 0 0 0 31 31 A 2\
+ 2 0 0 0 31 27 z\
+ M 19 33 C 17.89\
+5 33 17 33.895 1\
+7 35 C 17 36.105\
+ 17.895 37 19 37\
+ L 25 37 C 26.10\
+5 37 27 36.105 2\
+7 35 C 27 33.895\
+ 26.105 33 25 33\
+ L 19 33 z M 31 \
+33 A 2 2 0 0 0 3\
+1 37 A 2 2 0 0 0\
+ 31 33 z\x22/></svg\
+>\
+"
+
+qt_resource_name = b"\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x0d\
+\x07d\x0f\xa7\
+\x00b\
+\x00a\x00c\x00k\x00s\x00p\x00a\x00c\x00e\x00.\x00s\x00v\x00g\
+\x00\x08\
+\x08&W\xe7\
+\x00c\
+\x00a\x00l\x00c\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x02\x00\x00\x00\x03\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x8fD]i\xcc\
+\x00\x00\x000\x00\x00\x00\x00\x00\x01\x00\x00\x09|\
+\x00\x00\x01\x8fD]i\xe8\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
